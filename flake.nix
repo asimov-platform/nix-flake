904a7f78897643fe13e289c21614cd7f04882ed3
AlexSanches1 <sanches2026@gmail.com>
2025-03-25 15:51:25 -0400

    nix-ci

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
 
   outputs = { self, nixpkgs, ... }:
     let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+        };
+
       mkAsimov = { url, sha256, system, version }:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = pkgsFor system;
         in
         pkgs.stdenv.mkDerivation {
           pname = "asimov-cli";
@@ -38,31 +43,44 @@
         };
 
       version = "25.0.0-dev.3";
-    in {
-      packages.x86_64-darwin.default = mkAsimov {
-        system = "x86_64-darwin";
-        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-x86.gz";
-        sha256 = "f2620190d96c1d929ce1c43d6e7f6ba751b32b3000d6419c5f032e4e23a2dc3e";
-        inherit version;
-      };
-      packages.aarch64-darwin.default = mkAsimov {
-        system = "aarch64-darwin";
-        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-arm.gz";
-        sha256 = "c89a0660b1d091c332f6cf1611fe6f9021c8ed48948f2c43e2b05dfeb7afc8de";
-        inherit version;
-      };
-      packages.x86_64-linux.default = mkAsimov {
+
+      asimov-x86_64-linux = mkAsimov {
         system = "x86_64-linux";
         url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-x86.gz";
         sha256 = "0c30685a1b814364c17a70b629e3b94a24bec0c66c1943bd324cf56b57980326";
         inherit version;
       };
-      packages.aarch64-linux.default = mkAsimov {
+
+      asimov-aarch64-linux = mkAsimov {
         system = "aarch64-linux";
         url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-arm.gz";
         sha256 = "2761b82936598588b7266d6d63abd1271e57d087c14f382f1ab15e523973c3e5";
         inherit version;
       };
+
+      asimov-x86_64-darwin = mkAsimov {
+        system = "x86_64-darwin";
+        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-x86.gz";
+        sha256 = "f2620190d96c1d929ce1c43d6e7f6ba751b32b3000d6419c5f032e4e23a2dc3e";
+        inherit version;
+      };
+
+      asimov-aarch64-darwin = mkAsimov {
+        system = "aarch64-darwin";
+        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-arm.gz";
+        sha256 = "c89a0660b1d091c332f6cf1611fe6f9021c8ed48948f2c43e2b05dfeb7afc8de";
+        inherit version;
+      };
+    in
+    {
+      packages.x86_64-linux.default = asimov-x86_64-linux;
+      packages.aarch64-linux.default = asimov-aarch64-linux;
+      packages.x86_64-darwin.default = asimov-x86_64-darwin;
+      packages.aarch64-darwin.default = asimov-aarch64-darwin;
+
+      defaultPackage.x86_64-linux = asimov-x86_64-linux;
+      defaultPackage.aarch64-linux = asimov-aarch64-linux;
+      defaultPackage.x86_64-darwin = asimov-x86_64-darwin;
+      defaultPackage.aarch64-darwin = asimov-aarch64-darwin;
     };
 }
-
