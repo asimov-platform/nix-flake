c0b482634917b8f7b02c583f6a012971641a9cf4
Oleksandr Yakovenko <35838571+AlexSanches1@users.noreply.github.com>
2025-03-26 04:43:53 +0400

    Added test nix instalation via github action
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci
    
    * nix-ci

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
-  description = "Asimov CLI Flake with explicit per-platform packages";
+  description = "ASIMOV CLI Flake with explicit per-platform packages";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs, ... }:
     let
+      pkgsFor = system: import nixpkgs { inherit system; };
+
       mkAsimov = { url, sha256, system, version }:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = pkgsFor system;
         in
         pkgs.stdenv.mkDerivation {
           pname = "asimov-cli";
@@ -37,32 +39,44 @@
           doCheck = true;
         };
 
-      version = "25.0.0-dev.2";
-    in {
-      packages.x86_64-darwin.default = mkAsimov {
+      version = "25.0.0-dev.3";
+
+      asimov-x86_64-linux = mkAsimov {
+        system = "x86_64-linux";
+        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-x86-gnu.gz";
+        sha256 = "0c30685a1b814364c17a70b629e3b94a24bec0c66c1943bd324cf56b57980326";
+        inherit version;
+      };
+
+      asimov-aarch64-linux = mkAsimov {
+        system = "aarch64-linux";
+        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-arm-gnu.gz";
+        sha256 = "2761b82936598588b7266d6d63abd1271e57d087c14f382f1ab15e523973c3e5";
+        inherit version;
+      };
+
+      asimov-x86_64-darwin = mkAsimov {
         system = "x86_64-darwin";
         url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-x86.gz";
-        sha256 = "332f2a586bb3d702db60b165f09b96a01995034a35ab109713d01ba53f0cb2cf";
+        sha256 = "f2620190d96c1d929ce1c43d6e7f6ba751b32b3000d6419c5f032e4e23a2dc3e";
         inherit version;
       };
-      packages.aarch64-darwin.default = mkAsimov {
+
+      asimov-aarch64-darwin = mkAsimov {
         system = "aarch64-darwin";
         url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-macos-arm.gz";
-        sha256 = "af05128473c3dfda48e7740d2a456a7996a6391ac46a3ef0620cb803afaeb793";
+        sha256 = "c89a0660b1d091c332f6cf1611fe6f9021c8ed48948f2c43e2b05dfeb7afc8de";
         inherit version;
       };
-      packages.x86_64-linux.default = mkAsimov {
-        system = "x86_64-linux";
-        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-x86.gz";
-        sha256 = "a64251f846d0cc0280a7f83f25f0db0c4f15ef4caf349ccd45dad22250866060";
-        inherit version;
-      };
-      packages.aarch64-linux.default = mkAsimov {
-        system = "aarch64-linux";
-        url    = "https://github.com/asimov-platform/asimov-cli/releases/download/${version}/asimov-linux-arm.gz";
-        sha256 = "e9ca2393fe6147a01f10a36bd162b8f014fcfc1e964bb65a33e6d0920b34960d";
-        inherit version;
-      };
-    }
-}
+    in {
+      packages.x86_64-linux.default = asimov-x86_64-linux;
+      packages.aarch64-linux.default = asimov-aarch64-linux;
+      packages.x86_64-darwin.default = asimov-x86_64-darwin;
+      packages.aarch64-darwin.default = asimov-aarch64-darwin;
 
+      defaultPackage.x86_64-linux = asimov-x86_64-linux;
+      defaultPackage.aarch64-linux = asimov-aarch64-linux;
+      defaultPackage.x86_64-darwin = asimov-x86_64-darwin;
+      defaultPackage.aarch64-darwin = asimov-aarch64-darwin;
+    };
+}
